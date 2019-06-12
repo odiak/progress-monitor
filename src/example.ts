4b852b2a9812d61aaf6ad1315e6f1f1a09efc990
Kaido Iwamoto <h6824.kaido@gmail.com>
2019-06-12 14:01:23 +0900

    Make example.ts better

--- src/example.ts
+++ src/example.ts
@@ -13,7 +13,7 @@ function startLogging(variants: string[], i: number = 0) {
     if (t < 100) {
       logProgress({
         variant: v,
-        metrics: 'a',
+        metrics: 'metric 1',
         value: Math.sin(t / 20) + 0.1 * (Math.random() * 2 - 1),
         epoch: t,
         n_epochs: 100
@@ -23,7 +23,7 @@ function startLogging(variants: string[], i: number = 0) {
         const val = Math.sqrt(t) + 0.1 * (Math.random() * 2 - 1)
         logProgress({
           variant: v,
-          metrics: 'b',
+          metrics: 'metric 2',
           value: { x1: val, x2: val * (0.9 + Math.random() * 0.05) },
           epoch: t,
           n_epochs: 100
@@ -38,5 +38,5 @@ function startLogging(variants: string[], i: number = 0) {
 }
 
 setTimeout(() => {
-  startLogging(['A', 'B'])
+  startLogging(['variant A', 'variant B'])
 }, 4000)
