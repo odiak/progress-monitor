dcca0b98bdd84bfe9c6a00df1a6f39365754d35e
Kaido Iwamoto <h6824.kaido@gmail.com>
2019-06-12 12:50:39 +0900

    Add example program

---
+++ src/example.ts
@@ -0,0 +1,42 @@
+function logProgress(object: unknown) {
+  console.log(JSON.stringify(object))
+}
+
+function startLogging(variants: string[], i: number = 0) {
+  if (i >= variants.length) {
+    return
+  }
+  const v = variants[i]
+
+  let t = 0
+  const timer = setInterval(() => {
+    if (t < 100) {
+      logProgress({
+        variant: v,
+        metrics: 'a',
+        value: Math.sin(t / 20) + 0.1 * (Math.random() * 2 - 1),
+        epoch: t,
+        n_epochs: 100
+      })
+
+      if (t % 2 === 0) {
+        const val = Math.sqrt(t) + 0.1 * (Math.random() * 2 - 1)
+        logProgress({
+          variant: v,
+          metrics: 'b',
+          value: { x1: val, x2: val * (0.9 + Math.random() * 0.05) },
+          epoch: t,
+          n_epochs: 100
+        })
+      }
+    } else {
+      clearInterval(timer)
+      startLogging(variants, i + 1)
+    }
+    t++
+  }, 300)
+}
+
+setTimeout(() => {
+  startLogging(['A', 'B'])
+}, 4000)
